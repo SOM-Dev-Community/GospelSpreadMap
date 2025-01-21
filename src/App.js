16a5ee10681b2fc37f29cb8d1dc5fd9f3ec745a3
User-Err <prospermadojmeu00@gmail.com>
2025-01-21 21:31:50 +0100

    Merge Conflicts Resolved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
-import MapView from './components/map/MapView';
-=======
 import React from 'react';
 import Report from './pages/Report/Report';
->>>>>>> pages/reports-page
 import Login from "./login"; 
-import React from "react";
 import ReactDOM from "react-dom";
-
+import MapView from './components/map/MapView';
 
 
 
@@ -18,12 +13,9 @@ React.createElement("div", null, React.createElement(Login));
 function App() {
   return (
     <div className="App">
-<<<< HEAD
       <h1 className="title1" style={{ color: "white",  fontFamily: "Times new roman" }}>THE GOSPEL SPREAD MAP</h1>
       <MapView />
-=======
       <Report />
->>>>>>> pages/reports-page
     </div>
   );
 }
