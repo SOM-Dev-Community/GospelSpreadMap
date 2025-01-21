4c4194c53c44f25638ccfc8c79d314de47e77822
davidkablee <sirdavidudoji07@gmail.com>
2025-01-21 11:00:31 +0100

    Implement interactive map component Added Map component to display geographic data Integrated third-party map library (e.g., D3 Maps API) Enabled panning, zooming, and marker placement functionality Set up basic styling and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import MapView from './components/map/MapView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title1" style={{ color: "white",  fontFamily: "Times new roman" }}>THE GOSPEL SPREAD MAP</h1>
+      <MapView />
     </div>
   );
 }
