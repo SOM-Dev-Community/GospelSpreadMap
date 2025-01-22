2c0427fc87275f40908e83392b7f2c92be8ac279
Udoji David <85876572+DavidKablee@users.noreply.github.com>
2025-01-22 12:25:24 +0100

    Update App.js
    Removed the Title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ React.createElement("div", null, React.createElement(Login));
 function App() {
   return (
     <div className="App">
-      <h1 className="title1" style={{ color: "white",  fontFamily: "Times new roman" }}>THE GOSPEL SPREAD MAP</h1>
       <MapView />
       <Report />
     </div>
