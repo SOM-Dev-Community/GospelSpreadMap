d93521241a84d193d9e4bbf74ecbcf22c084292b
User-Err <prospermadojmeu00@gmail.com>
2025-01-26 12:15:40 +0100

    file-naming-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import SignUp from "../src/pages/SignUp/SignUp.jsx"; // Import the Signup component
+import SignUp from "./Pages/SignUp/SignUp.jsx"; // Import the Signup component
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import React Router
-import Home from '../src/pages/Home/Home.jsx';
-import Login from '../src/pages/Login/Login.jsx';
-import Reports from '../src/Pages/Reports/Reports.jsx';
+import Home from './Pages/Home/Home.jsx';
+import Login from './Pages/Login/Login.jsx';
+import Reports from './Pages/Reports/Reports.jsx';
 
 function App() {
   const [open, setOpen] = React.useState(false);
