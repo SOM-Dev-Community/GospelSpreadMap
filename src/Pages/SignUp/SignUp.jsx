5cd99fa49378fdea9b8c9c422f4aeb20afae732b
Udoji David <85876572+DavidKablee@users.noreply.github.com>
2025-02-03 21:47:23 +0100

    Update SignUp.jsx
    Fixed the broken link to the login page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function SignUp() {
@@ -86,7 +87,7 @@ function SignUp() {
         </button>
         <div className="helper-links">
           <p>
-            Already have an account? <a href="/login">Log in here</a>
+            <Link to="/login">Already have an account? Login</Link>
           </p>
         </div>
       </form>
