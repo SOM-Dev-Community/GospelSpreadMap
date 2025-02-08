cba601d37373814a415cbb71060674afdf46aa0e
davidkablee <sirdavidudoji07@gmail.com>
2025-02-08 02:24:14 +0100

    Modified the PasswordReset page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,39 +1,91 @@
-import React, { useEffect } from 'react'
-import './Home.css'
-import { Button, Divider, Typography, useMediaQuery } from '@mui/material'
-import { green, blue } from '@mui/material/colors'
-import NavigationMenu from '../../components/NavigationMenu/NavigationMenu'
-import logo from '../../assets/GSMAPLOGO.png'
-import GlobeComponent from '../../components/GlobeComponent/GlobeComponent'
+import React, { useEffect } from "react";
+import "./Home.css";
+import { Button, Divider, Typography, useMediaQuery } from "@mui/material";
+import { green, blue } from "@mui/material/colors";
+import NavigationMenu from "../../components/NavigationMenu/NavigationMenu";
+import logo from "../../assets/GSMAPLOGO.png";
+import GlobeComponent from "../../components/GlobeComponent/GlobeComponent";
+import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  const [visible, setVisible] = React.useState(true)
+  const [visible, setVisible] = React.useState(true);
   const matches = useMediaQuery("(min-width:768px)");
-  useEffect(()=> {
-    setVisible(matches)
-  }, [matches])
+  useEffect(() => {
+    setVisible(matches);
+  }, [matches]);
   return (
-    <div className='Home'>
-      <nav className='home-navbar'>
-        <NavigationMenu visible= {visible} setVisible= {setVisible}/>
-        <img src={logo} alt='  GSMAP' className='logo'/>
+    <div className="Home">
+      <nav className="home-navbar">
+        <NavigationMenu visible={visible} setVisible={setVisible} />
+        <img src={logo} alt="  GSMAP" className="logo" />
       </nav>
-      <GlobeComponent/> 
-      <div className={visible ? 'reach-percentage' : 'reach-percentage hidden'}>
-        <Typography sx={{fontSize: 52, fontStretch: 'expanded', fontFamily: "Smooch sans", lineHeight: "1.1", fontWeight: 500}}>60%</Typography>
-        <Typography sx={{fontSize: 18, fontStretch: 'expanded', fontFamily: "Smooch sans", lineHeight: "1", fontWeight: 500, textTransform:"capitalize"}}>spread rate of the gospel around the world</Typography>
+      <GlobeComponent />
+      <div className={visible ? "reach-percentage" : "reach-percentage hidden"}>
+        <Typography
+          sx={{
+            fontSize: 52,
+            fontStretch: "expanded",
+            fontFamily: "Smooch sans",
+            lineHeight: "1.1",
+            fontWeight: 500,
+          }}
+        >
+          60%
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: 18,
+            fontStretch: "expanded",
+            fontFamily: "Smooch sans",
+            lineHeight: "1",
+            fontWeight: 500,
+            textTransform: "capitalize",
+          }}
+        >
+          spread rate of the gospel around the world
+        </Typography>
       </div>
-      <div className={visible ? 'color-codes-info' : 'color-codes-info hidden'}> 
-        <Typography className='color-codes' sx={{padding: "5px", fontSize: 14}}>Where the Gospel is yet to reach</Typography>
-        <Divider/>
-        <Typography className='color-codes' sx={{padding: "5px", fontSize: 14}}>Where the Gospel has reached</Typography>
+      <div className={visible ? "color-codes-info" : "color-codes-info hidden"}>
+        <Typography
+          className="color-codes"
+          sx={{ padding: "5px", fontSize: 14 }}
+        >
+          Where the Gospel is yet to reach
+        </Typography>
+        <Divider />
+        <Typography
+          className="color-codes"
+          sx={{ padding: "5px", fontSize: 14 }}
+        >
+          Where the Gospel has reached
+        </Typography>
       </div>
-      <Button sx={{flexDirection:"column", backgroundColor: blue[800], color: 'black', position: "fixed", bottom: "5%", marginLeft: "10px", marginRight: "10px", padding: "10px", borderRadius: "10px", fontSize: 12, boxShadow: "none", alignItems: "center", justifyContent: "center"}}>
-        <Typography sx={{fontWeight: 800, lineHeight: 1.1, fontSize: 16}}>CLICK TO UPLOAD YOUR REPORT IF YOU HAVE EVANGELIZED</Typography>
-        <Typography sx={{fontWeight: 600, fontSize:12}}>STREET, TOWN, CITY, STATE, COUNTRY FOR THE GOSPEL</Typography>
+      <Button
+        sx={{
+          flexDirection: "column",
+          backgroundColor: blue[800],
+          color: "black",
+          position: "fixed",
+          bottom: "5%",
+          marginLeft: "10px",
+          marginRight: "10px",
+          padding: "10px",
+          borderRadius: "10px",
+          fontSize: 12,
+          boxShadow: "none",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography sx={{ fontWeight: 800, lineHeight: 1.1, fontSize: 16 }}>
+          CLICK TO UPLOAD YOUR REPORT IF YOU HAVE EVANGELIZED
+        </Typography>
+        <Typography sx={{ fontWeight: 600, fontSize: 12 }}>
+          STREET, TOWN, CITY, STATE, COUNTRY FOR THE GOSPEL
+        </Typography>
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Pages/PasswordReset/PasswordReset.jsx
+++ src/Pages/PasswordReset/PasswordReset.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 import "../PasswordReset/PasswordReset.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
 
 function PasswordReset() {
   const [email, setEmail] = useState("");
@@ -11,18 +14,39 @@ function PasswordReset() {
     e.preventDefault();
     console.log("Login Info:", { email, password });
 
-
     // Simulate successful login
     // After performing any login checks or API calls, redirect to the report page
     // navigate("App");
   };
 
-  
   return (
     <div className="container">
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{
+          position: "absolute",
+          top: "5%",
+          right: "2%",
+          backgroundColor: "rgba(48, 48, 48, 0.1)",
+          color: "blue",
+          padding: "10px",
+          borderRadius: "10px",
+          fontSize: 12,
+          boxShadow: "none",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ArrowBackIosIcon sx={{ fontSize: 12 }} />
+        <Link to="/" className="Link">
+          Back to Home
+        </Link>
+      </Button>
+
       {/* Left Panel */}
       <div className="left-panel">
-      <img
+        <img
           src="https://d1z1smzgvvydhp.cloudfront.net/MnCiW3vZW58mLWAiMNIj0zdtWtc=/1874x968/smart/filters:format(webp)/https://cdn1.kingschat.online/uploads/media/5393857a73312e31b57f3700/dVc2V2dOeVBYR2tkZkQ3YlNVUkY1dz09/GS_MAP_LOGO.png"
           alt="Gospel Spread Map Logo"
           className="left-panel-logo"
@@ -38,7 +62,6 @@ function PasswordReset() {
       {/* Right Panel */}
       <div className="login-form">
         {/* Logo */}
-        
 
         {/* Login Form */}
         <h2>Reset Password</h2>
@@ -54,7 +77,6 @@ function PasswordReset() {
             />
           </div>
 
-
           {/* Login Button */}
           <button className="login-btn" type="submit">
             Reset Password
@@ -62,14 +84,14 @@ function PasswordReset() {
         </form>
 
         {/* Helper Links */}
-        {/* <div className="helper-links">
-          <p>
+        <div className="helper-links">
+          {/*<p>
             Already have an account? <Link to="/login">Login here</Link>
-          </p>
+          </p>*/}
           <p>
-          Don't have an account?,<Link to="/signup"> Sign up now!</Link>
+            Don't have an account?,<Link to="/signup"> Sign up now!</Link>
           </p>
-        </div> */}
+        </div>
       </div>
     </div>
   );
